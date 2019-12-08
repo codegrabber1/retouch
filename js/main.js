9b4239fd8d169d638005e68846158a5c5426b7e5
codegrabber1 <poruchenko@ukr.net>
2019-12-08 23:32:19 +0200

    created mobile menu, add menu at about page

--- js/main.js
+++ js/main.js
@@ -1,29 +1,52 @@
 jQuery(document).ready(function($){
+
   $("#main-carousel").owlCarousel({
     items: 1,
     loop: true,
     autoplay: true
   });
+  // top menu
+  $(".sf-menu").superfish();
+  $(".sf-menu").after("<div id='my-menu'>");
+  $(".sf-menu").clone().appendTo("#my-menu");
+  $("#my-menu").find("*").attr('style', '');
+  $("#my-menu").find("ul").removeClass("sf-menu");
+  $("#my-menu").mmenu({
+    extensions: ["widescreen", "pagedim-black", "effect-menu-slide", "effect-listitems-slide", "fx-menu-zoom", "fx-panels-zoom", "theme-dark"],
+    navbar: {
+      title: "Western Rehabilitation and Sports Center"
+    }
+  });
+  var api = $("#my-menu").data("mmenu");
+  api.bind("closed", function() {
+    $(".toggle-mnu").removeClass("on");
+  });
 
-  // $('.grid_container').masonry({
-  // // options
-  // //initLayout: false,
-  // itemSelector: '.grid-item',
-  // columnWidth: '.grid_size',
-  // //percentPosition: true,
-  // gutter: '.gutter-sizer',
-  // fitWidth: true,
-  // resize: true
-  // });
-});
+  $(".mobile-mnu").click(function() {
+    var mmAPI = $("#my-menu").data("mmenu");
+    mmAPI.open();
+    var thiss = $(this).find(".toggle-mnu");
+    mmAPI.bind("open:finish", function() {
+      thiss.addClass("on");
+    });
 
-var elem = document.querySelector('.grid_container');
-var msnry = new Masonry( elem, {
-  // options
+    mmAPI.bind("close:finish", function() {
+      thiss.removeClass("on");
+    });
+
+    $(".main-mnu").slideToggle();
+    return false;
+  });
+  // end top menu
+}); // end ready
+// init Masonry
+var $grid = $('.grid').masonry({
   itemSelector: '.grid-item',
-  columnWidth: '.grid_size',
-  // //percentPosition: true,
-  // gutter: '.gutter-sizer',
-  // fitWidth: true,
-  // resize: true
+  percentPosition: true,
+  columnWidth: '.grid-sizer',
+  resize: true
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry();
 });
